82ca0930e35c0db9f3afd6d789e273f8d9ffeee2
M4X1MUS <61908477+M4X1MUS07@users.noreply.github.com>
2024-12-10 23:31:48 +0545

    allowed all origins

--- src/index.js
+++ src/index.js
@@ -4,24 +4,30 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import { checkApiStatus, saveApiStatus } from "./status-checker.js";
 import ApiStatus from "./api-status-schema.js";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
+app.use(cors());
 const port = process.env.PORT || 3002;
 
 const mongoURI = process.env.MONGO_URI;
 
-mongoose.connect(mongoURI).then(async () => {
+mongoose
+  .connect(mongoURI)
+  .then(async () => {
     console.log("Connected to MongoDB");
 
     const db = mongoose.connection.db;
     const collections = await db.listCollections().toArray();
-    const collectionExists = collections.some(collection => collection.name === 'ApiStatus');
+    const collectionExists = collections.some(
+      (collection) => collection.name === "ApiStatus"
+    );
 
     if (!collectionExists) {
-        console.log("ApiStatus collection does not exist. Creating it now.");
-        await ApiStatus.createCollection();
+      console.log("ApiStatus collection does not exist. Creating it now.");
+      await ApiStatus.createCollection();
     }
 
     console.log("Running initial API status check...");
@@ -29,23 +35,24 @@ mongoose.connect(mongoURI).then(async () => {
     await saveApiStatus(initialApiStatuses);
 
     cron.schedule("0 * * * *", async () => {
-        console.log("Checking API status...");
-        const apiStatuses = await checkApiStatus();
-        await saveApiStatus(apiStatuses);
+      console.log("Checking API status...");
+      const apiStatuses = await checkApiStatus();
+      await saveApiStatus(apiStatuses);
     });
     console.log("API status checker started.");
 
     app.get("/api/status", async (req, res) => {
-        try {
-            const apiStatuses = await ApiStatus.find({});
-            res.json(apiStatuses);
-        } catch (err) {
-            console.error("Error fetching API status:", err);
-            res.status(500).json({ error: "Failed to fetch API status" });
-        }
+      try {
+        const apiStatuses = await ApiStatus.find({});
+        res.json(apiStatuses);
+      } catch (err) {
+        console.error("Error fetching API status:", err);
+        res.status(500).json({ error: "Failed to fetch API status" });
+      }
     });
 
     app.listen(port, () => {
-        console.log(`API status server listening on port ${port}`);
+      console.log(`API status server listening on port ${port}`);
     });
-}).catch((error) => console.error("MongoDB connection error:", error));
+  })
+  .catch((error) => console.error("MongoDB connection error:", error));
