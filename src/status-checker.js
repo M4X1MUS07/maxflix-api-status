54b1c5fc438f65da713df939adf0162291be3ee4
M4X1MUS07 <itzmerm07@gmail.com>
2025-01-23 05:14:24 +0545

    remove afmx

--- src/status-checker.js
+++ src/status-checker.js
@@ -18,7 +18,7 @@ const routes = [
     { name: "RSMX", url: "/rsmx/scrape", params: { type: "movie", tmdbId: "299534" } },
     { name: "TEMX (Movies only)", url: "/temx/scrape", params: { type: "movie", tmdbId: "299534" } },
     { name: "SPMX", url: "/spmx/scrape", params: { type: "movie", tmdbId: "299534" } },
-    { name: "AFMX", url: "/afmx/scrape", params: { type: "movie", tmdbId: "299534", title: "Avengers: Endgame", releaseYear: 2019 } },
+    // { name: "AFMX", url: "/afmx/scrape", params: { type: "movie", tmdbId: "299534", title: "Avengers: Endgame", releaseYear: 2019 } },
     { name: "AEMX", url: "/aemx/scrape", params: { type: "movie", tmdbId: "299534" } },
     // { name: "CFMX", url: "/cfmx/scrape", params: { type: "movie", tmdbId: "299534", title: "avengers endgame", releaseYear: 2019 } },
     // { name: "ESMX", url: "/esmx/scrape", params: { type: "movie", tmdbId: "299534" } },
